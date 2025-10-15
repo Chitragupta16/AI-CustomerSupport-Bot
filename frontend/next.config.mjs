73c4c2683fe0d142b5e9d2f9ca304ba27255a26e
Soumyashis Sarkar <118597505+Chitragupta16@users.noreply.github.com>
2025-10-15 12:14:04 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,16 @@
+import path from "path"
+
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname)
+    return config
+  },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
