8f69a098196c947bea51a85dee380a0d7e9ae5d0
Soumyashis Sarkar <118597505+Chitragupta16@users.noreply.github.com>
2025-10-15 11:47:35 +0530

    Update use-toast.ts

--- frontend/hooks/use-toast.ts
+++ frontend/hooks/use-toast.ts
@@ -3,6 +3,7 @@
 export function useToast() {
   const toast = (msg: { title?: string; description?: string }) => {
     console.log("Toast:", msg.title || "", msg.description || "")
+    alert(`${msg.title ?? ""}\n${msg.description ?? ""}`)
   }
   return { toast }
 }
